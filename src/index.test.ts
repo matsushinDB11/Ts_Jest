cda5fdf3cddb5ff51a09e8d29f54632489e20af7
matsushinDB11 <matsushin35@gmail.com>
2021-12-08 18:10:25 +0900

    add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,3 +7,19 @@ test('plus test', ()=> {
     }
     expect<parsedOutput>(formulaParser('2+3')).toStrictEqual(data);
 })
+
+test('minus test', ()=> {
+    const data: parsedOutput = {
+        numbers: [2, 3],
+        symbols: ['-']
+    }
+    expect<parsedOutput>(formulaParser('2-3')).toStrictEqual(data);
+})
+
+test('2桁', ()=> {
+    const data: parsedOutput = {
+        numbers: [23, 98],
+        symbols: ['+']
+    }
+    expect<parsedOutput>(formulaParser('23+98')).toStrictEqual(data);
+})
